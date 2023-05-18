f568f43c3c27a83efd2662f2d13e9f90f626b47d
almudenarp <almuramosplatero@gmail.com>
2023-05-18 23:54:58 +0200

    actualizacion menu :)

--- Main.c
+++ Main.c
@@ -1,12 +1,72 @@
 #include <stdio.h>
+#include <conio.h>
 
-int main()
-{
-    printf ("\nPrueba\n\n");
+void pInicio() {
+    printf("\n");
+    printf("\tBienvenido al programa de calculos estadisticos\n\n");
+}
 
-    printf ("Prueba 2");
+void pSeleccion() {
+    printf("\n");
+    printf("\tSeleccione la operacion a realizar:\n\n");
+    printf("\t1\tCalculos estadisticos\n");
+    printf("\t2\tDatos actuales\n");
+    printf("\t3\tIngresar datos\n");
+    printf("\t4\tEliminar datos\n");
+    printf("\t5\tSalir del programa\n\n");
+}
 
-    printf ("Prueba 3");
+void pCalculosEstadisticos() {
+    printf("\n");
+    printf("\tSeleccione el rango temporal:\n\n");
+    printf("\t1\tAnual\n");
+    printf("\t2\tAnual - Mensual\n");
+    printf("\t3\tMensual\n");
+    printf("\t4\tTotal\n\n");
+}
 
-    return 0;
+void pVerDatos() {
+    printf("\n");
+    printf("\t1\tVer datos por mes - ano\n");
+    printf("\t3\tVer datos por mes\n");
+    printf("\t3\tVer datos por ano\n");
+    printf("\t4\tOrdenar valores\n\n");
+}
+
+void pIngresarFecha() {
+    printf("\n");
+    printf("\tIngrese fecha:\n\n");
+}
+
+void pSalir() {
+    printf("\n");
+    printf("\tSeleccione una opcion:\n\n");
+    printf("\t1\tGuardar cambios\n");
+    printf("\t2\tNo guardar cambios\n");
+    printf("\t3\tCancelar\n\n");
+}
+
+void pConfirmarSalir() {
+    printf("\n");
+    printf("\tSeguro que quiere salir?\n\n");
+    printf("\t1\tSi\n");
+    printf("\t2\tNo\n\n");
+}
+
+void tipoIngreso() {
+    printf("\n");
+    printf("\tSeleccione la forma en la que quiere ingresar los datos:\n\n");
+    printf("\t1\tDocumento en la misma carpeta que el programa\n");
+    printf("\t2\tIngreso manual o copia pega\n");
+    printf("\t3\tAtras\n\n");
+}
+
+void ingresoNombreDocumento() {
+    printf("\n");
+    printf("\tIntroduzca el nombre del documento:\n\n\t");
+}
+
+void ingresoCopiaDatos() {
+    printf("\n");
+    printf("\tIntroduzca los datos tal y como aparecen en el fichero:\n\n\t");
 }
