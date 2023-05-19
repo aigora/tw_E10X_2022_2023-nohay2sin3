e3195ea66b6a872be9d1c742a708ab0ed1df6d1c
almudenarp <almuramosplatero@gmail.com>
2023-05-19 16:26:06 +0200

    correcion de error

--- main.c
+++ main.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "menu.h"
 
 int main() {
+    //Declaracion de variables necesarias
     int entrada;
+    int i, j, k;
+    char filas[23][10000], fila[10000];
+
+    FILE *archivo;
 
     //Inicio con la pantalla de bienvenida
     system("cls");
     pInicio();
     system("pause");
     system("cls");
+
+    //Apertura de fichero
+    archivo = fopen("documento.txt", "r");
+    if (archivo == NULL) {
+        printf("No se ha podido abrir el archivo");
+        return 0;
+    }
+
+    //Lectura de documento.txt
+    for (i = 0; i < 23; i++) {
+        fgets(fila, 10000, archivo);
+        strcpy(filas[i], fila);
+        memset(fila, 0, 10000);
+    }
+    fclose(archivo);
+
 
     pantallaSeleccion:
         pSeleccion();
@@ -20,20 +43,6 @@ int main() {
             case '1':
                 //Caso calculos estadisticos
                 system("cls");
-                    //Apertura de fichero
-                archivo = fopen("documento.txt", "r");
-                if (archivo == NULL) {
-                    printf("No se ha podido abrir el archivo");
-                    return 0;
-                }
-
-                //Lectura de documento.txt
-                for (i = 0; i < 23; i++) {
-                    fgets(fila, 10000, archivo);
-                    strcpy(filas[i], fila);
-                    memset(fila, 0, 10000);
-                }
-                fclose(archivo);
                 break;
 
             case '2':
