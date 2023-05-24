dd6c7498c228538707f00d328aa3f95d130bb8f1
felipetafurth <felipe.tafurth@alumnos.upm.es>
2023-05-24 18:20:02 +0200

    nota funcion calculo

--- leerFichero.h
+++ leerFichero.h
@@ -41,3 +41,5 @@ void escoger_fichero_salida(char filas[][10000], int numYear, int num_mes_por_ye
 void guardar_datos(char filas[][10000], int numYear, int num_mes_por_year[], datos GWh[]);
 
 void eliminar_dato(datos GWh[], int numYear, int num_mes_por_year[]);
+//FUNCIONES PARA CALCULOS
+//Nota, si el dato es eliminado = 1 o calculable = 0, no hay que tenerlo en cuenta para los calculos, para los datos totales no uses la funcion de contar datos, ya que no se añaden comas en las primeras filas
